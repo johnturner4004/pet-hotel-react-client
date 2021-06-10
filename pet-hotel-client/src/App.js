d199c596c98add3252bfbbe9b67c34da1a9b5874
Robyn Noble <robynhurd09@gmail.com>
2021-06-10 16:01:34 -0500

    preparing to merge

--- pet-hotel-client/src/App.js
+++ pet-hotel-client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 import Home from './Home/Home'
+import AddPet from './AddPet/AddPet.jsx';
 
 function App() {
   return (
       <Router>
         <Route exact='/' to='home'>
           <Home />
+        </Route>
+        <Route exact='/addPet'>
+          <AddPet />
         </Route>
       </Router>
 
