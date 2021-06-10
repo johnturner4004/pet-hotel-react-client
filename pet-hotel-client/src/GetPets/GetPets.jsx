8866fce2ef476aa291de2b7bc8140bf6a5febccf
Robyn Noble <robynhurd09@gmail.com>
2021-06-10 16:15:42 -0500

    updated link headers

--- pet-hotel-client/src/GetPets/GetPets.jsx
+++ pet-hotel-client/src/GetPets/GetPets.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import AddPet from '../AddPet/AddPet.jsx';
 
 function GetPets() {
   const dispatch = useDispatch();
@@ -19,6 +20,7 @@ function GetPets() {
     <>
       <h1>Pet's List</h1>
       {JSON.stringify(petList)}
+      <AddPet />
       {/* <ul>
         {petList.map((pet) => (
           <li key={pet.id}>{pet.name}</li>
