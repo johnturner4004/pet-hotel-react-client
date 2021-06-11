3bd322920a3ddbe1ef2c3bd5ad6b718b037f3672
johnturner4004 <johnturner4004@gmail.com>
2021-06-11 11:48:18 -0400

    displayed all data from pet list

--- pet-hotel-client/src/GetPets/GetPets.jsx
+++ pet-hotel-client/src/GetPets/GetPets.jsx
@@ -1,5 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  Paper, 
+  TableContainer, 
+  TableHead, 
+  TableRow, 
+  TableCell, 
+  Table, 
+  TableBody
+} from '@material-ui/core';
 
 function GetPets() {
   const dispatch = useDispatch();
@@ -18,12 +27,40 @@ function GetPets() {
       //
     <>
       <h1>Pet's List</h1>
-      {JSON.stringify(petList)}
       {/* <ul>
-        {petList.map((pet) => (
+        {petList.map(pet => (
           <li key={pet.id}>{pet.name}</li>
         ))}
       </ul> */}
+      <Paper>
+        <TableContainer>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell>Owner</TableCell>
+                <TableCell>Pet</TableCell>
+                <TableCell>Breed</TableCell>
+                <TableCell>Color</TableCell>
+                <TableCell>Checked In</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {petList ? petList.map(pet => {
+                return(
+                <TableRow key={pet.id}>
+                  <TableCell>{pet.name}</TableCell>
+                  <TableCell>{pet.pet}</TableCell>
+                  <TableCell>{pet.breed}</TableCell>
+                  <TableCell>{pet.color}</TableCell>
+                  <TableCell>check-in</TableCell>
+                  <TableCell>buttons</TableCell>
+                </TableRow>
+              )}):''}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </>
   );
 }
